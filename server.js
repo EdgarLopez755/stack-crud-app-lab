4f13c3a4c4502623baae537d5c980486488b524d
Edgar Lopez <edgarlopez755@gmail.com>
2024-09-15 14:07:27 -0700

    Connect to MongoDB

--- server.js
+++ server.js
@@ -1,35 +1,37 @@
+const dotenv = require('dotenv')
+dotenv.config()
 
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const mongoose = require('mongoose')
 
 
-app.get('/test', (req, res) => {
-  res.send("testing server")
-})
-
-app.get('/', async (req, res) => {
-  res.render('index.ejs')
-})
-
-
 const foodSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  image: String,
-})
+  name: String,
+  isReadyToEat: Boolean,
+});
+ 
+    
+process.env.MONGODB_URI='mongodb+srv://edgarlopez755:zHGPeUzilXAYqOYc@student-cluster.z2jsf.mongodb.net/student-cluster?retryWrites=true&w=majority&appName=Student-cluster'
+    mongoose.connection.on('connected', () => {
+        console.log('Connected to MongoDB ')
+    })
 
+mongoose.connect(process.env.MONGODB_URI)
+    
 const Food = mongoose.model('Food', foodSchema)
 
-module.exports = Food
+module.exports = Food;
 
-
-
-
-
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+app.get('/test', (req,res) => {
+    res.send('Testing')
 });
 
+app.get('/', async(req, res) => {
+    res.render('index.ejs')
+});
 
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+  });
 
