ce8916a4be18a66356d6b992112bce6c6b1737e9
Edgar Lopez <edgarlopez755@gmail.com>
2024-09-15 16:44:46 -0700

    Add index.ejs/Views

--- server.js
+++ server.js
@@ -6,30 +6,31 @@ const app = express();
 const mongoose = require('mongoose')
 
 
-const foodSchema = new mongoose.Schema({
-  name: String,
-  isReadyToEat: Boolean,
-});
- 
-    
+
 process.env.MONGODB_URI='mongodb+srv://edgarlopez755:zHGPeUzilXAYqOYc@student-cluster.z2jsf.mongodb.net/student-cluster?retryWrites=true&w=majority&appName=Student-cluster'
     mongoose.connection.on('connected', () => {
         console.log('Connected to MongoDB ')
     })
-
-mongoose.connect(process.env.MONGODB_URI)
     
-const Food = mongoose.model('Food', foodSchema)
 
-module.exports = Food;
-
-app.get('/test', (req,res) => {
-    res.send('Testing')
-});
+mongoose.connect(process.env.MONGODB_URI)   
+// const Food = mongoose.model('Food', foodSchema)
+// module.exports = Food
+const Food = require('./models/food.js')
 
 app.get('/', async(req, res) => {
     res.render('index.ejs')
 });
+
+app.get('/foods', async(req, res) => {
+    const allFoods = await Food.find()
+    console.log(allFoods)
+    res.render('foods/index.ejs', {foods: allFoods})
+})
+
+app.get('/foods/new', (req, res) => {
+    res.render('foods/new.ejs')
+})
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
